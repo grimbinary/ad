9c14e4579422cc19a151f785779132d12dd1c3f2
grimbinary <rightstoneflask@gmail.com>
2023-07-14 11:44:46 -0700

    Added code to be able to "randomly" generate users

--- code/gen_ad.ps1
+++ code/gen_ad.ps1
@@ -7,6 +7,12 @@ function CreateADGroup(){
     New-ADGroup -name $name -GroupScope Global
 }
 
+function RemoveADGroup(){
+    param( [Parameter(Mandatory=$true)] $groupObject )
+
+    $name = $groupObject.name
+    Remove-ADGroup -Identity $name -Confirm:$False
+}
 function CreateADUser() {
     param( [Parameter(Mandatory=$true)] $userObject )
 
@@ -37,6 +43,15 @@ function CreateADUser() {
     }
 
 }
+
+function WeakenPasswordPolicy(){
+    secedit /export /cfg c:\Windows\Tasks\secpol.cfg
+    (Get-Content c:\Windows\Tasks\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File c:\Windows\Tasks\secpol.cfg
+    secedit /configure /db c:\windows\security\local.sdb /cfg c:\Windows\Tasks\secpol.cfg /areas SECURITYPOLICY
+    rm -force c:\Windows\Tasks\secpol.cfg -confirm:$false
+}
+
+WeakenPasswordPolicy
 
 $json = (Get-Content $JSONFile | ConvertFrom-Json)
 
