a1c8fedc5fb31f5b14379c828d07d15e99fa8fed
grimbinary <rightstoneflask@gmail.com>
2023-07-17 22:19:30 -0700

    Updatedx AD generator code to create local admin accounts on the DC

--- code/random_domain.ps1
+++ code/random_domain.ps1
@@ -1,38 +1,71 @@
-param( [parameter(Mandatory=$true)] $OutputJSONFile )
+param( 
+    [Parameter(Mandatory=$true)] $OutputJSONFile,
+    [int]$UserCount,
+    [int]$GroupCount,
+    [int]$LocalAdminCount
+ )
 
-$group_names = [System.Collections.ArrayList] (Get-Content "data/group_names.txt")
-$first_names = [System.Collections.ArrayList] (Get-Content "data/first_names.txt")
-$last_names = [System.Collections.ArrayList] (Get-Content "data/last_names.txt")
-$passwords = [System.Collections.ArrayList] (Get-Content "data/passwords.txt")
+
+$group_names = [System.Collections.ArrayList](Get-Content "data/group_names.txt")
+$first_names = [System.Collections.ArrayList](Get-Content "data/first_names.txt")
+$last_names = [System.Collections.ArrayList](Get-Content "data/last_names.txt")
+$passwords = [System.Collections.ArrayList](Get-Content "data/passwords.txt")
 
 $groups = @()
 $users = @()
 
-$num_groups = 10 
-for ( $i = 0; $i -lt $num_groups; $i++ ) {
+# Default UserCount set to 5 (if not set)
+if ( $UserCount -eq 0 ){
+    $UserCount = 5
+}
+
+# Default GroupCount set to 5 (if not set)
+if ( $GroupCount -eq 0 ){
+    $GroupCount = 1
+}
+
+if ( $LocalAdminCount -ne 0){
+    $local_admin_indexes = @()
+    while (($local_admin_indexes | Measure-Object ).Count -lt $LocalAdminCount){
+        
+        $random_index = (Get-Random -InputObject (1..($UserCount)) | Where-Object { $local_admin_indexes -notcontains $_ } )
+        $local_admin_indexes += @( $random_index )
+        echo "adding $random_index to local_admin_indexes $local_admin_indexes"
+    }
+}
+
+for ( $i = 1; $i -le $GroupCount; $i++ ){
     $group_name = (Get-Random -InputObject $group_names)
-    $group = @{"name"="$group_name" }
+    $group = @{ "name" = "$group_name" }
     $groups += $group
     $group_names.Remove($group_name)
 }
 
-$num_users = 100
-for ( $i = 0; $i -lt $num_users; $i++ ) {
-    $first_name =(Get-Random -InputObject $first_names)
-    $last_name =(Get-Random -InputObject $last_names)
-    $password =(Get-Random -InputObject $passwords)
-    $new_user = @{
+for ( $i = 1; $i -le $UserCount; $i++ ){
+    $first_name = (Get-Random -InputObject $first_names)
+    $last_name = (Get-Random -InputObject $last_names)
+    $password = (Get-Random -InputObject $passwords)
+
+    $new_user = @{ `
         "name"="$first_name $last_name"
         "password"="$password"
-        "groups"=@( (Get-Random -InputObject $groups).name )
+        "groups" = (Get-Random -InputObject $groups).name
+        }
+
+    if ( $local_admin_indexes | Where { $_ -eq $i  } ){
+        echo "user $i is local admin"
+        $new_user["local_admin"] = $true
     }
+
     $users += $new_user
+
     $first_names.Remove($first_name)
     $last_names.Remove($last_name)
     $passwords.Remove($password)
 }
 
-@{ "domain" = "xyz.com" 
-        #"groups"=$groups
-        "users"=$users
-} | ConvertTo-JSON | Out-File $OutputJSONFile
+ConvertTo-Json -InputObject @{ 
+    "domain"= "xyz.com"
+    "groups"=$groups
+    "users"=$users
+} | Out-File $OutputJSONFile 
